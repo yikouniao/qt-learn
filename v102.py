f86fa003d8ae3186b90afc3ebda62da3bfc50c3b
C&T <chinoandtippi@gmail.com>
2018-03-15 20:59:06 +0800

    增加停止功能

--- v102.py
+++ v102.py
@@ -54,9 +54,10 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.mcmot_thread.mcmot_signal2.connect(self.update_mcmot_frame2)
 
         #任老师的结束
-        #self.End_1.clicked.connect(自己加吧)
+        self.End_1.clicked.connect(self.mot_thread.terminate)
         #梁总的结束
-        #self.End_2.clicked.connect(自己加吧)
+        self.End_2.clicked.connect(self.mcmot_thread.terminate)
+
     def Open_file_txt(self):
         '''set the multi-object tracking detection directory. '''
         self.mot_thread.mot_det_dir, _ = QtWidgets.QFileDialog.getOpenFileName(self,
@@ -197,11 +198,11 @@ class MCMOT(QtCore.QThread):
         super(MCMOT, self).__init__(parent)
 
     def run(self):
-        if self.mcmot_video_dir1.split('/')[-1] != '1.mp4' or self.mcmot_video_dir2.split('/')[-1] != '2.mp4':
-            print('Unmatched Video Files And Detection Files!\n')
-            return
         if self.mcmot_det_dir1.split('/')[-1] != 'demoData1.mat' or self.mcmot_det_dir2.split('/')[-1] != 'demoData2.mat':
             print('UUnmatched Video Files And Detection Files!\n')
+            return
+        if self.mcmot_video_dir1.split('/')[-1] != '1.mp4' or self.mcmot_video_dir2.split('/')[-1] != '2.mp4':
+            print('Unmatched Video Files And Detection Files!\n')
             return
 
         # load videos.
